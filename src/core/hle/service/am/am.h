8b7d145812aef51939fec1e5b0c39d2620b780ae
PabloMK7 <hackyglitch2@gmail.com>
2025-07-16 20:38:59 +0200

    Z3DS: Add title info and smdh to metadata
    # Conflicts:
    #	src/core/hle/service/am/am.cpp
    #	src/core/hle/service/am/am.h

--- src/core/hle/service/am/am.h
+++ src/core/hle/service/am/am.h
@@ -115,6 +115,15 @@ struct ImportContentContext {
 };
 static_assert(sizeof(ImportContentContext) == 0x18, "Invalid ImportContentContext size");
 
+struct TitleInfo {
+    u64_le tid;
+    u64_le size;
+    u16_le version;
+    u16_le unused;
+    u32_le type;
+};
+static_assert(sizeof(TitleInfo) == 0x18, "Title info structure size is wrong");
+
 struct CTCert {
     u32_be signature_type{};
     std::array<u8, 0x1E> signature_r{};
@@ -376,6 +385,11 @@ InstallStatus InstallCIA(const std::string& path,
  */
 InstallStatus CheckCIAToInstall(const std::string& path, bool& is_compressed,
                                 bool check_encryption);
+
+/**
+ * Get CIA metadata information from file.
+ */
+ResultVal<std::pair<TitleInfo, std::unique_ptr<Loader::SMDH>>> GetCIAInfos(const std::string& path);
 
 /**
  * Downloads and installs title form the Nintendo Update Service.
