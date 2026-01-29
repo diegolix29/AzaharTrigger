175f901447e958efb0821029354145a19c0266b1
PabloMK7 <hackyglitch2@gmail.com>
2026-01-29 13:10:02 +0100

    frontend: Revert removal of .3ds support (#1701)
    * frontend: Revert removal of .3ds support
    
    * Added 3ds extension to Info.plist
    
    * Added .3ds extension to org.azahar_emu.Azahar.xml
    
    * game_list.h: Removed leftover definitions
    
    ---------
    
    Co-authored-by: OpenSauce04 <opensauce04@gmail.com>
    # Conflicts:
    #	src/android/app/src/main/java/org/citra/citra_emu/fragments/GamesFragment.kt
    #	src/android/app/src/main/java/org/citra/citra_emu/model/Game.kt
    #	src/citra_qt/citra_qt.cpp
    #	src/citra_qt/game_list.cpp
    #	src/citra_qt/uisettings.h
    #	src/core/loader/loader.cpp

--- src/android/app/src/main/java/org/citra/citra_emu/model/Game.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/model/Game.kt
@@ -68,7 +68,7 @@ class Game(
         val allExtensions: Set<String> get() = extensions + badExtensions
 
         val extensions: Set<String> = HashSet(
-            listOf("3ds", "3dsx", "elf", "axf", "cci", "cxi", "app", "z3dsx", "zcci", "zcxi")
+            listOf("3dsx", "app", "axf", "cci", "cxi", "elf", "z3dsx", "zcci", "zcxi", "3ds")
         )
 
         val badExtensions: Set<String> = HashSet(
