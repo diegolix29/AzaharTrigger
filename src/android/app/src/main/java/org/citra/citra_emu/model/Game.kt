71092dc4b4d0658384486938be5bf499b9354d51
OpenSauce04 <opensauce04@gmail.com>
2026-03-14 19:21:54 +0000

    android: Fixed installed app shortcut creation failing on vanilla

--- src/android/app/src/main/java/org/citra/citra_emu/model/Game.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/model/Game.kt
@@ -9,11 +9,16 @@ package org.citra.citra_emu.model
 import android.os.Parcelable
 import android.content.Intent
 import android.net.Uri
+import androidx.core.net.toUri
+import java.io.File
+import java.io.IOException
 import java.util.HashSet
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 import org.citra.citra_emu.CitraApplication
+import org.citra.citra_emu.NativeLibrary
 import org.citra.citra_emu.activities.EmulationActivity
+import org.citra.citra_emu.utils.BuildUtil
 
 @Parcelize
 @Serializable
@@ -39,12 +44,25 @@ class Game(
     val keyLastPlayedTime get() = "${filename}_LastPlayed"
 
     val launchIntent: Intent
-        get() = Intent(CitraApplication.appContext, EmulationActivity::class.java).apply {
-            action = Intent.ACTION_VIEW
-            data = if (isInstalled) {
-                CitraApplication.documentsTree.getUri(path)
+        get() {
+            var appUri: Uri
+            if (isInstalled) {
+                if (BuildUtil.isGooglePlayBuild) {
+                    appUri = CitraApplication.documentsTree.getUri(path)
+                } else {
+                    val nativePath = NativeLibrary.getUserDirectory() + "/" + path
+                    val nativeFile = File(nativePath)
+                    if (!nativeFile.exists()) {
+                        throw IOException("Attempting to create shortcut for an executable that doesn't exist: $nativePath")
+                    }
+                    appUri = Uri.fromFile(nativeFile)
+                }
             } else {
-                Uri.parse(path)
+                appUri = path.toUri()
+            }
+            return Intent(CitraApplication.appContext, EmulationActivity::class.java).apply {
+                action = Intent.ACTION_VIEW
+                data = appUri
             }
         }
 
